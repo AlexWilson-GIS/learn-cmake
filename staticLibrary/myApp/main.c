43e954b5343cbccadbfdc99086b4a274d3db5c6c
Alex Wilson <awilson@esri.com>
2024-10-07 12:43:59 -0400

    Completed first two exercises

---
+++ staticLibrary/myApp/main.c
@@ -0,0 +1,10 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include "../myLibrary/simpleMath.h"
+
+int main() {
+  
+  printf("2 + 2 = %i\n", addInts(2, 2));
+
+  return EXIT_SUCCESS;
+}
---
+++ staticLibrary/myLibrary/simpleMath.c
@@ -0,0 +1,13 @@
+#include "simpleMath.h"
+
+int addInts(int a, int b) {
+  return a + b;
+}
+
+float addFloats(float a, float b) {
+  return a + b;
+}
+
+double addDoubles(double a, double b) {
+  return a + b;
+}
